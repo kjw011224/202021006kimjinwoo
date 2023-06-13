fc38b5d2699cb774f1595374bed47232366219ca
kjw011224 <136270854+kjw011224@users.noreply.github.com>
2023-06-13 15:34:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as pltimport numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 import streamlit as st
